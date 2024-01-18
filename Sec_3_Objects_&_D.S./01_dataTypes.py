440f6129e5c5bf1e217a99c64dac566d9152c236
MP-30 <adityabhadauriya93@gmail.com>
2024-01-19 00:12:35 +0530

    dataTypes & numbers

---
+++ Sec_3_Objects_&_D.S./01_dataTypes.py
@@ -0,0 +1,8 @@
+a = 3
+print(a)
+b = [{'owner': 'blackstone@moneybloom.in'},
+ {'owner': 'everest@moneybloom.in'},
+ {'owner': 'shrisai@moneybloom.in'}]
+print(type(b))
+c = {'owner': 'blackstone@moneybloom.in'}
+print(type(c))
---
+++ Sec_3_Objects_&_D.S./02_numbers.py
@@ -0,0 +1,10 @@
+a = 2+1
+print(a)
+b = 3/5
+print(b)
+c = 26%9
+print(c)
+d = 2**4
+print(d)
+e = 2+10 *10+3
+print(e)
