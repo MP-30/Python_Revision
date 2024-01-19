986c6c6fadabd728067b32ee2fd1b83842794b2f
MP-30 <adityabhadauriya93@gmail.com>
2024-01-20 00:17:54 +0530

    variable assingment

---
+++ Sec_3_Objects_&_D.S./03_variable_assingments.py
@@ -0,0 +1,16 @@
+a = 5
+print(a)
+b = a +a
+print(b)
+b = 4
+print(b)
+b = b+b
+print(b)
+print(type (b))
+inte  = 4
+print(inte)
+
+my_income = 100
+tax_rate = 0.1
+my_taxes = my_income * tax_rate
+print(my_taxes)
