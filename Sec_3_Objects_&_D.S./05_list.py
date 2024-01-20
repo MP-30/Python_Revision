de495bb9d2d602ba09a84967db344b4651dc298b
MP-30 <adityabhadauriya93@gmail.com>
2024-01-20 23:16:37 +0530

    list

---
+++ Sec_3_Objects_&_D.S./05_list.py
@@ -0,0 +1,33 @@
+list = [1,2,2,3,4,5]
+print(list)
+
+my_list = ["String", 100,0.3,False]
+print(len(my_list))
+mylist = ['one','two','three']
+print(mylist[0])
+
+print(mylist + my_list)
+
+mylist[0] = "One"
+print(mylist) 
+mylist.append("four")
+print(mylist)
+mylist.append("five")
+print(mylist)
+
+print(mylist.pop())
+print(mylist)
+
+print(mylist.pop(1))
+print(mylist)
+new_list = [1,2,3,4,5,6,7,8]
+print(new_list.pop(1))
+
+new_lst = ['a','e','q','r','y']
+new_lst.sort()
+a = new_lst
+print(a)
+
+new_lst.reverse()
+b =new_lst
+print(b)
