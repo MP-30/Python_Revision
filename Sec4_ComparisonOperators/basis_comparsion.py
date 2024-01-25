ab6afd68724dbcbd6516f1ad1082b4d4f0996b7d
MP-30 <adityabhadauriya93@gmail.com>
2024-01-25 12:42:19 +0530

    comparasion operators

---
+++ Sec4_ComparisonOperators/basis_comparsion.py
@@ -0,0 +1,15 @@
+print(2==3)
+print(2==2)
+print('Bye'=='bye')
+print(2.0==2)
+print(4!=5)
+print(4>3)
+a = 1<3 and 4<5
+print("a value")
+print(a)
+b = 1 > 3 or 4>5
+print("b value")
+print(b)
+
+c = not (1 > 3) 
+print(c)
