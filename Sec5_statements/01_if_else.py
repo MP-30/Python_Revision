d797a537247169a4c452fc76e3cb971eae2f8061
MP-30 <adityabhadauriya93@gmail.com>
2024-01-26 23:07:44 +0530

    Basics_if_else

---
+++ Sec5_statements/01_if_else.py
@@ -0,0 +1,14 @@
+hungry = True
+if hungry:
+  print("Feed me")
+  
+else:
+  print("Dont feed me")
+  
+loc = 'Bank'
+
+if loc == 'Store':
+  print("Cars are cool")
+elif loc == 'Bank': print("Money is cool")
+elif loc == 'Store': print("Stores are cool")
+else: print("Not cool")
