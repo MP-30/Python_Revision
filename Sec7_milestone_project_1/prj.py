ca1f1cce1fd3d40101070d24e2f3e2e39a3507d0
MP-30 <adityabhadauriya93@gmail.com>
2024-02-04 22:26:48 +0530

    part_1 of project complete

--- Sec7_milestone_project_1/prj.py
+++ Sec7_milestone_project_1/prj.py
@@ -13,7 +13,24 @@ display(row1, row2, row3)
 # position_index = int(input("Choose an index position: "))
 
 def user_choice():
-    choice = input("Please enter a number(0-10): ")
-    return choice.isdigit()
+    choice= 'Wrong'
+    accepted_value = range (0,11)
+    within_range = False
+    
 
+    # Digit pr Within range
+    while choice.isdigit() == False or within_range == False:
+        choice = input("Please enter a number(0-10): ")
+        # Digit Check
+        if choice.isdigit() == False:
+            print("Sorry this is not a digit")
+        # Range Check
+        if choice.isdigit() == True:
+            if int(choice) in accepted_value:
+                within_range = True
+            else :
+                print("Out of range")
+                within_range = False
+    return int(choice)
+ 
 print(user_choice())
