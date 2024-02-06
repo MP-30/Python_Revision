b5304cbc0928f6a06cd6c7e3eb41ae25a40f6778
MP-30 <adityabhadauriya93@gmail.com>
2024-02-06 21:40:00 +0530

    little progress in project

--- Sec7_milestone_project_1/First_proj.py
+++ Sec7_milestone_project_1/First_proj.py
@@ -1,3 +1,4 @@
+'''
 board = ['1','2','3','4','5','6','7','8','9']
 marker = ''
 player1 = ''
@@ -45,8 +46,37 @@ def update_value_inList():
     board[player1_position] = player2 
     
     
-def final_func():
-    display_board(board)
-    user_input()
-    user_position()
-    update_value_inList()
+def check_result():
+    if (board[0] == board [1] == board [2] 
+        or board[3] == board [4] == board [5] 
+        or board[6] == board [7] == board [8]
+        or board[0] == board [3] == board [6]
+        or board[1] == board [4] == board [7]
+        or board[2] == board [5] == board [8]
+        or board[0] == board [4] == board [8]
+        or board[2] == board [4] == board [6]):
+        return ('Somebody won')
+    else: pass
+def final_game():
+    
+'''
+board = ['#','X','O','X','O','X','O','X','O','X']
+
+from IPython.display import clear_output
+ 
+def display_board(board):
+    clear_output()
+    print(f'{board[7]} | {board[8]} | {board[9]}')
+    print(f'{board[4]} | {board[5]} | {board[6]}')
+    print(f'{board[1]} | {board[2]} | {board[3]}')
+display_board(board)
+
+def player_input():
+    marker = ''
+    while marker != 'X' and marker != 'O':
+        marker = input('Player 1, Choose X or O: ')
+        
+    if marker == 'X':
+        return ('X', 'O')
+    elif marker == 'O':
+        return ('O', 'X')
