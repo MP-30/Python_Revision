4a1d98f70f2790a223794a7ed4797cf879cfa493
MP-30 <adityabhadauriya93@gmail.com>
2024-02-19 17:38:22 +0530

    objects basics

--- Oop/Objects_instances/objects.py
+++ Oop/Objects_instances/objects.py
@@ -1,2 +1,26 @@
 class House:
-    pass
+    def __init__(self, price):
+        self.price = price
+        
+        
+class Backpack:
+    def __init__(self, color, size):
+        self.items = []
+        self.color = color
+        self.size = size
+        
+class Circle:
+    def __init__(self, radius) -> None:
+        self.radius = radius
+
+class Rectangle:
+    def __init__(self, length, width) -> None:
+        self.length = length
+        self.width = width
+
+class Movie:
+    def __init__(self, title, year, language, rating) -> None:
+        self.title = title
+        self.year = year
+        self.language = language
+        self.rating = rating
