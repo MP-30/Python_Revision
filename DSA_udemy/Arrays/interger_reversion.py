e8372fd7464cd178c96c55a818ab545290fe5999
MP-30 <adityabhadauriya93@gmail.com>
2024-06-05 22:11:06 +0530

    array problm

---
+++ DSA_udemy/Arrays/interger_reversion.py
@@ -0,0 +1,13 @@
+
+def reverse_integer(n):
+    reversed_int = 0
+    remainder = 0
+    while(n>0):
+        remainder = n % 10
+        reversed_int = reversed_int * 10 + remainder
+        n = n//10 
+
+    return reversed_int
+
+if  __name__ == '__main__':
+    print(reverse_integer(7123457240))
--- DSA_udemy/Arrays/palindrome.py
+++ DSA_udemy/Arrays/palindrome.py
@@ -1,18 +1,14 @@
 
 def reverse(strr):
-    start_point = 0
-    end_point = len(strr) -1
-    print(end_point)
-    print(strr[1])
-    while end_point > start_point:
-        strr[start_point], strr[end_point] = strr[end_point], strr[start_point]
-        start_point =+ start_point +1
-        end_point =+ end_point -1
-
+    s = strr[::-1]
+    if s == strr:
+        return True
+    else:
+        return False
 if __name__ == '__main__':
     str1 = 'radar'
     str2 = 'aditya'
     str3 = 'madam'
     a = reverse(str1)
-    print(reverse(str1))
-    print(a)
+    print(reverse(str3))
+    
